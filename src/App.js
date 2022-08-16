f52016cf62992a8fefaeb159b06ead296581e98f
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-16 12:22:13 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./components/Content";
+import Connexion from "./components/Connexion";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="connexion" element={<Connexion />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
