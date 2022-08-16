475f6968af7261a165b965741d43930f041c466c
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-16 11:56:17 +0200

    Typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import "./Header.css";
-import logo from "./img/logo1.png";
 
 export default function Header() {
   return (
@@ -7,15 +6,18 @@ export default function Header() {
       <nav id="navp">
         <div id="logoNav">
           <a href="index.php?page=accueil">
-            <img src={logo} alt="logo de petstore  " />
+            <img src="./assets/img/logo1.png" alt="logo de petstore  " />
           </a>
         </div>
         <div>
+          {/* <div className="container"></div> */}
           <ul id="headMenu">
-            <div className="container">
-              <input type="search" placeholder="Rechercher ..." />
-              <button type="submit">Search</button>
-            </div>
+            <li>
+              <div>
+                <input type="search" placeholder="Rechercher ..." />
+                <button type="submit">Search</button>
+              </div>
+            </li>
             <li>
               <a href="index.php?page=accueil">
                 <div className="icon">
@@ -34,17 +36,6 @@ export default function Header() {
                 </div>
                 <div>
                   <span>Contact</span>
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a href="">
-                <div className="icon">
-                  <i className="fa-solid fa-basket-shopping fa-2xl"></i>
-                </div>
-                <div>
-                  <span>Panier</span>
                 </div>
               </a>
             </li>
