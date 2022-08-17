95f5a7b57649ac8c76f684eca54c6fe269bd5ab6
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-17 15:08:04 +0200

    Restructuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,69 +1,72 @@
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <nav id="navp">
-        <div id="logoNav">
-          <Link to="/">
-            <img src="./assets/img/logo1.png" alt="logo de petstore  " />
-          </Link>
-        </div>
-        <div>
-          {/* <div className="container"></div> */}
-          <ul id="headMenu">
-            <li>
-              <div>
-                <input type="search" placeholder="Rechercher ..." />
-                <button type="submit">Search</button>
-              </div>
-            </li>
-            <li>
-              <Link to="/">
-                <div className="icon">
-                  <i className="fa-solid fa-house fa-2xl"></i>
-                </div>
+    <>
+      <header>
+        <nav id="navp">
+          <div id="logoNav">
+            <Link to="/">
+              <img src="./assets/img/logo1.png" alt="logo de petstore  " />
+            </Link>
+          </div>
+          <div>
+            {/* <div className="container"></div> */}
+            <ul id="headMenu">
+              <li>
                 <div>
-                  <span>Home</span>
+                  <input type="search" placeholder="Rechercher ..." />
+                  <button type="submit">Search</button>
                 </div>
-              </Link>
-            </li>
+              </li>
+              <li>
+                <Link to="/">
+                  <div className="icon">
+                    <i className="fa-solid fa-house fa-2xl"></i>
+                  </div>
+                  <div>
+                    <span>Home</span>
+                  </div>
+                </Link>
+              </li>
 
-            <li>
-              <Link to="/contact">
-                <div className="icon">
-                  <i className="fa-solid fa-phone fa-2xl"></i>
-                </div>
-                <div>
-                  <span>Contact</span>
-                </div>
-              </Link>
-            </li>
+              <li>
+                <Link to="/contact">
+                  <div className="icon">
+                    <i className="fa-solid fa-phone fa-2xl"></i>
+                  </div>
+                  <div>
+                    <span>Contact</span>
+                  </div>
+                </Link>
+              </li>
 
-            <li>
-              <Link to="/connexion">
-                <div className="icon">
-                  <i className="fa-solid fa-user fa-2xl"></i>
-                </div>
-                <div>
-                  <span>Connexion</span>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <a href="">
-                <div className="icon">
-                  <i className="fa-solid fa-basket-shopping fa-2xl"></i>
-                </div>
-                <div>
-                  <span>Panier</span>
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+              <li>
+                <Link to="/connexion">
+                  <div className="icon">
+                    <i className="fa-solid fa-user fa-2xl"></i>
+                  </div>
+                  <div>
+                    <span>Connexion</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <a href="">
+                  <div className="icon">
+                    <i className="fa-solid fa-basket-shopping fa-2xl"></i>
+                  </div>
+                  <div>
+                    <span>Panier</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./components/style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Connexion from "./components/Connexion";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Inscription from "./components/Inscription";
+import Layout from "./components/Layout";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="connexion" element={<Connexion />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="inscription" element={<Inscription />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
